51069a8a57aad1fe0fb7ca7c9c5b64a937ec4d2b
vladproduction <vladbogdantsev@gmail.com>
2025-06-17 12:51:26 +0300

    refactor

--- Java-Multithreads-Base/src/main/java/com/vladproduction/ch0_training/staticnestedclass/Main.java
+++
@@ -1,8 +0,0 @@
-package com.vladproduction.ch0_training.staticnestedclass;
-
-public class Main {
-    public static void main(String[] args) {
-        OuterClass.StaticNestedClass nestedObject = new OuterClass.StaticNestedClass();
-        nestedObject.display();
-    }
-}
