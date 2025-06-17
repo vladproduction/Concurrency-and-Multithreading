28d3eb385b9448d20a551ef983bb01bf89915877
VladProduction <124387608+vladproduction@users.noreply.github.com>
2025-06-17 12:20:23 +0300

    Delete Java-Threads directory

--- Java-Threads/src/main/java/com/vladproduction/ch01_thread_and_concurrency/MainThread.java
+++
@@ -1,9 +0,0 @@
-package com.vladproduction.ch01_thread_and_concurrency;
-
-public class MainThread {
-    public static void main(String[] args) {
-
-        System.out.println(Thread.currentThread().getName());
-
-    }
-}
